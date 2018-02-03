07b3efa220127b7134caca7eed718fcaa7a9f644
Nial Daly <ndaly500@hotmail.co.uk>
2018-02-03 14:43:21 +0000

    first commit

--- app.py
+++ app.py
@@ -29,4 +29,6 @@ def logout():
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
-    app.run(debug=True,host='0.0.0.0', port=4000)
+    app.debug = True
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
